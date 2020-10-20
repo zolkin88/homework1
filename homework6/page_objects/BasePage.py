a882ee3b6b65ecffbc2287c506bafb24f5b2a2ac
diman <z0lking@yandex.ru>
2020-10-20 20:18:30 +0300

    selenoid +loging+allure

--- homework6/page_objects/BasePage.py
+++ homework6/page_objects/BasePage.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+import logging
 
 
 class BasePage(object):
@@ -10,23 +11,35 @@ class BasePage(object):
 
     def __init__(self, driver):
         self.driver = driver
+        self.logger = logging.getLogger(type(self).__name__)
+        self.logger.setLevel(logging.DEBUG)
+        self.ch = logging.FileHandler('opencart.log')
+        self.formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        self.ch.setFormatter(self.formatter)
+        self.logger.addHandler(self.ch)
 
     def _input(self, locator, value):
+        self.logger.info(u'Ждем появления элемента ' + locator[1])
         element = WebDriverWait(self.driver, self.TIME_TO_WAIT).until(
             EC.presence_of_element_located(locator))
+        self.logger.info(u'Очищаем элемент ' + locator[1])
         element.clear()
+        self.logger.info(u'Вводим в элемент значение ' + value)
         element.send_keys(value)
 
     def _click_element(self, locator):
+        self.logger.info(u'Нажимаем на элемент ' + locator[1])
         WebDriverWait(self.driver, self.TIME_TO_WAIT).until(
             EC.element_to_be_clickable(locator)).click()
 
     def _get_element_text_by_attr(self, locator, attr):
+        self.logger.info(u'Получаем значение атрибута {0} у локатора {1} '.format(attr, locator[1]))
         element = WebDriverWait(self.driver, self.TIME_TO_WAIT).until(
             EC.presence_of_element_located(locator))
         return element.get_attribute(attr)
 
     def _get_element_text(self, locator):
+        self.logger.info(u'Получаем текст у локатора {0} '.format(locator[1]))
         element = WebDriverWait(self.driver, self.TIME_TO_WAIT).until(
             EC.presence_of_element_located(locator))
         return element.text
--- homework6/tests/conftest.py
+++ homework6/tests/conftest.py
@@ -1,12 +1,28 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver import FirefoxOptions, ChromeOptions
+import logging
+import pytest
+from selenium.webdriver.support.events import EventFiringWebDriver, AbstractEventListener
+from .common import MyListener
+
+
+@pytest.fixture
+def create_logger():
+    logger = logging.getLogger('test_console_logger')
+    ch = logging.StreamHandler()
+    logger.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+    return logger
 
 
 def pytest_addoption(parser):
     parser.addoption("--browser", default="firefox")
     parser.addoption("--base_url", default="http://localhost")
     parser.addoption("--path", default="/admin")
+    parser.addoption("--selenoid", action="store", default="127.0.0.1")
 
 
 @pytest.fixture
@@ -23,20 +39,32 @@ def get_path(request):
 
 @pytest.fixture
 def browser(request, get_base_url, get_path):
+    selenoid = request.config.getoption("--selenoid")
+    executor_url = f"http://{selenoid}:4444/wd/hub"
     if "chrome" == request.config.getoption("--browser"):
         chrome_options = ChromeOptions
+        caps = {"browserName": request.config.getoption("--browser")
+                }
         chrome_options.headless = True
         driver = webdriver.Chrome(executable_path='./chromedriver')
         driver.maximize_window()
+        driver = EventFiringWebDriver(webdriver.Chrome(), MyListener())
+        driver = webdriver.Remote(command_executor=executor_url, desired_capabilities=caps)
     else:
         firefox_options = FirefoxOptions()
         firefox_options.headless = False
         driver = webdriver.Firefox(executable_path='./geckodriver',
                                    options=firefox_options)
         driver.maximize_window()
+        driver = EventFiringWebDriver(driver, MyListener())
+        #Раскоментировать если нужно запуск на selenoid
+        # caps = {"browserName": request.config.getoption("--browser")
+        #         }
+        # driver = webdriver.Remote(command_executor=executor_url, desired_capabilities=caps)
 
     def open(path=get_path):
         return driver.get(get_base_url + path)
+
     driver.open = open
     driver.open()
     yield driver
